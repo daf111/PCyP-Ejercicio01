a62f9e6774f093ea4e3ff1504a3216c0725c7422
Daniel Alejandro Fernandez <daf111@yahoo.com.ar>
2020-03-12 20:54:09 -0300

    Impresión de Hola Mundo!
    Es para evitar 7 años de mala suerte

--- Ejercicio01/Ejercicio01/Program.cs
+++ Ejercicio01/Ejercicio01/Program.cs
@@ -10,6 +10,8 @@ namespace Ejercicio01
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hola mundo!");
+            Console.ReadLine();
         }
     }
 }
